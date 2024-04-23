f7af0a999b01f46378b60eb63bef803e599c675a
derekinn <38166940+derekinn@users.noreply.github.com>
2024-04-23 16:38:03 -0300

    Upload do spot

---
+++ script.js
@@ -0,0 +1,202 @@
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
+
+//musicas aq
+const VidaCara = {
+    songName: 'Vida Cara',
+    artist : 'Orochi, Domlaike, Chefin',
+    file: 'vida_cara'
+};
+const Junho94 = {
+    songName: 'Junho de 94',
+    artist : 'Djonga',
+    file: 'omqqsd'
+};
+const TTF = {
+    songName: 'Trap the fato',
+    artist : 'Derek, Leviano, Brandão085',
+    file: 'ttf'
+};
+let isPlaying = false;
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [
+  VidaCara,
+  Junho94,
+  TTF,
+];
+let sortedPlaylist = [...originalPlaylist];
+let index = 0;
+
+function playSong() {
+  play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+  play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+  song.play();
+  isPlaying = true;
+}
+
+function pauseSong() {
+  play.querySelector('.bi').classList.add('bi-play-circle-fill');
+  play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+  song.pause();
+  isPlaying = false;
+}
+
+function playPauseDecider() {
+  if (isPlaying === true) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+}
+
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked === true) {
+    likeButton.querySelector('.bi').classList.remove('bi-heart');
+    likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+    likeButton.classList.add('button-active');
+  } else {
+    likeButton.querySelector('.bi').classList.add('bi-heart');
+    likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+    likeButton.classList.remove('button-active');
+  }
+}
+
+function initializeSong() {
+  cover.src = `imgs/${sortedPlaylist[index].file}.jpg`;
+  song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender();
+}
+
+function previousSong() {
+  if (index === 0) {
+    index = sortedPlaylist.length - 1;
+  } else {
+    index -= 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function nextSong() {
+  if (index === sortedPlaylist.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function updateProgress() {
+  const barWidth = (song.currentTime / song.duration) * 100;
+  currentProgress.style.setProperty('--progress', `${barWidth}%`);
+  songTime.innerText = toHHMMSS(song.currentTime);
+}
+
+function jumpTo(event) {
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition / width) * song.duration;
+  song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+  let currentIndex = preShuffleArray.length;
+  let temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+    preShuffleArray[randomIndex] = temporaryValue;
+  }
+  if (preShuffleArray[0] === sortedPlaylist[index]) {
+    temporaryValue = preShuffleArray[0];
+    preShuffleArray[0] = preShuffleArray[preShuffleArray.length - 1];
+    preShuffleArray[preShuffleArray.length - 1] = temporaryValue;
+  }
+}
+
+function shuffleButtonClicked() {
+  if (isShuffled === false) {
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add('button-active');
+  } else {
+    isShuffled = false;
+    sortedPlaylist = [...originalPlaylist];
+    shuffleButton.classList.remove('button-active');
+  }
+}
+
+function repeatButtonClicked() {
+  if (repeatOn === false) {
+    repeatOn = true;
+    repeatButton.classList.add('button-active');
+  } else {
+    repeatOn = false;
+    repeatButton.classList.remove('button-active');
+  }
+}
+
+function nextOrRepeat() {
+  if (repeatOn === false) {
+    nextSong();
+  } else {
+    playSong();
+  }
+}
+
+function toHHMMSS(originalNumber) {
+  let hours = Math.floor(originalNumber / 3600);
+  let min = Math.floor((originalNumber - hours * 3600) / 60);
+  let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+  console.log('hours: ', hours);
+
+  return `${hours !== 0 ? hours.toString().padStart(2, '0') + ':' : ''}${min
+    .toString()
+    .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime() {
+  totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked() {
+  if (sortedPlaylist[index].liked === false) {
+    sortedPlaylist[index].liked = true;
+  } else {
+    sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+  localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
+}
+
+initializeSong();
+
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgress);
+song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
